421e886dea191b81e9baa13b8355bbd69151d3d7
DistantDig <jameswaller711@gmail.com>
2023-11-28 11:39:20 -0800

    webpack config and service worker file

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,1 +1,14 @@
 // TODO: Create a service worker that caches static assets:
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        this.navigator.serviceWorker.register('./src-sw.js').then(function(reistration) {
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function (err) {
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
+
+this.addEventListener('fetch', function (event) {
+
+});
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = () => {
   return {
@@ -11,17 +12,56 @@ module.exports = () => {
 
     // TODO: Add the correct output
     output: {
-      
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true
     },
 
     // TODO: Add the correct plugins
     plugins: [
-     
+      new HtmlWebpackPlugin({
+        title: 'Contacter',
+        template: './index.html'
+      }),
+
+      new GenerateSW(),
+      new WebpackPwaManifest({
+        name: 'Contact Directory',
+        short_name: 'Contacter',
+        description: 'Literally anything',
+        background_color: '7eb4e2',
+        theme_color: '7eb4e2',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('src', 'icons')
+          }
+        ]
+      })
     ],
 
     // TODO: Add the correct modules
     module: {
-
+      rules: [
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime']
+            }
+          }
+        }
+      ]
     }
-  };
+  }
 };
