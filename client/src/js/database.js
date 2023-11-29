c387137a271b3e65a334d4d689e5d9dd5bf1ff67
DistantDig <jameswaller711@gmail.com>
2023-11-29 10:06:29 -0800

    added initdb function

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -3,7 +3,16 @@ import { openDB } from 'idb';
 
 // TODO: Complete the initDb() function below:
 const initdb = async () => {
-
+    openDB('jate', 1, {
+        upgrade(db) {
+            if (db.objectStoreNames.contains('jate')) {
+                console.log('jate database already exists');
+                return;
+            }
+            db.createObjectStore('jate', {keyPath: 'id', autoIncrement: true });
+            console.log('jate database created');
+        }
+    })
 };
 
 
